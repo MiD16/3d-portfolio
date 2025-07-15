ebe3454b89d10d1d0daf64e80d7b8d1bb66fad64
Milad Khnefes <milad.khnefes.16@gmail.com>
2025-07-15 18:16:17 +0300

    About Me - Grids 3 & 4

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,5 +1,7 @@
 import React, { useRef } from "react";
 import Card from '../components/Card'
+import { Globe } from "../components/Globe";
+import CopyEmailButton from "../components/CopyEmailButton";
 
 const About = () => {
     const grid2Container = useRef();
@@ -8,7 +10,7 @@ const About = () => {
     <section className="c-space section-spacing">
       <h2 className="text-heading">About Me</h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12">
-        {/* Grid 1 */}
+        {/* Grid 1 - Introduction */}
         <div className="flex items-end grid-default-color grid-1">
           <img
             className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
@@ -21,7 +23,7 @@ const About = () => {
           </div>
           <div className="absolute inset-x-0 pointer-events-none -bottom-4 h-1/2 sm:h=1/3 bg-gradient-to-t from-indigo"/>
         </div>
-        {/* Grid 2 */}
+        {/* Grid 2 - Skills */}
         <div className="grid-default-color grid-2">
             <div className="flex items-center justify-center w-full h-full" ref={grid2Container}>
                 <p className="flex items-end text-5xl text-gray-500">Code is Craft</p>
@@ -35,9 +37,29 @@ const About = () => {
                 <Card style={{rotate: '-45deg', top: '5%', left: '10%'}} image='assets/logos/blazor-pink.png' containerRef={grid2Container}/>
             </div>
         </div>
-        
-        <div className="grid-black-color grid-3"></div>
-        <div className="grid-special-color grid-4"></div>
+        {/* Grid 3 - Location */}
+        <div className="grid-black-color grid-3">
+            <div className="z-10 w-[50%]">
+                <p className="headtext">
+                    Time Zone
+                </p>
+                <p className="subtext">
+                    I'm based in Syria, and open to remote work worldwide
+                </p>
+                <figure className="absolute left-[30%] top-[10%]">
+                    <Globe/>
+                </figure>
+            </div>
+        </div>
+        {/* Grid 4 - Call to Action */}
+        <div className="grid-special-color grid-4">
+            <div className="flex flex-col items-center justify-center gap-4 size-full">
+                <p className="text-center headtext">
+                    Do you want to start a Project together?
+                </p>
+                <CopyEmailButton/>
+            </div>
+        </div>
         <div className="grid-default-color grid-5"></div>
       </div>
     </section>
