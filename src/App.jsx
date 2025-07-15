2f4f1667259daa837dc543fcbe271b50f546e900
Milad Khnefes <milad.khnefes.16@gmail.com>
2025-07-15 21:08:33 +0300

    Finished Work Experience Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
 import About from './sections/About'
 import Projects from './sections/Projects'
+import Experiences from './sections/Experiences'
 
 function App() {
 
@@ -13,12 +14,10 @@ function App() {
         <Hero/>
         <About/>
         <Projects/>
+        <Experiences/>
         <section className='min-h-screen'></section>
         <section className='min-h-screen'></section>
         <section className='min-h-screen'></section>
-        <section className='min-h-screen'></section>
-        <section className='min-h-screen'></section>
-        {/* experience */}
         {/* testimonial */}
         {/* contact */}
         {/* footer */}
