aebdd6d08e2e67cb02e0f92a378a76ee05d463a5
Milad Khnefes <milad.khnefes.16@gmail.com>
2025-07-15 17:40:19 +0300

    About - Grids 1 & 2

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
 import Card from '../components/Card'
 
 const About = () => {
+    const grid2Container = useRef();
+
   return (
     <section className="c-space section-spacing">
       <h2 className="text-heading">About Me</h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12">
+        {/* Grid 1 */}
         <div className="flex items-end grid-default-color grid-1">
           <img
             className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
@@ -18,20 +21,21 @@ const About = () => {
           </div>
           <div className="absolute inset-x-0 pointer-events-none -bottom-4 h-1/2 sm:h=1/3 bg-gradient-to-t from-indigo"/>
         </div>
-        
+        {/* Grid 2 */}
         <div className="grid-default-color grid-2">
-            <div className="flex items-center justify-center w-full h-full">
+            <div className="flex items-center justify-center w-full h-full" ref={grid2Container}>
                 <p className="flex items-end text-5xl text-gray-500">Code is Craft</p>
-                <Card style={{rotate: '75deg', top: '30%', left: '20%'}} text='SOLID'/>
-                <Card style={{rotate: '-30deg', top: '60%', left: '45%'}} text='GRASP'/>
-                <Card style={{rotate: '90deg', top: '30%', left: '70%'}} text='DB'/>
-                <Card style={{rotate: '-45deg', top: '55%', left: '0%'}} text='API'/>
-                <Card style={{rotate: '20deg', top: '10%', left: '30%'}} text='Design Patterns'/>
-                <Card style={{rotate: '30deg', top: '70%', left: '70%'}} image='assets/logos/csharp-pink.png'/>
-                <Card style={{rotate: '-45deg', top: '70%', left: '25%'}} image='assets/logos/dotnet-pink.png'/>
-                <Card style={{rotate: '-45deg', top: '5%', left: '10%'}} image='assets/logos/blazor-pink.png'/>
+                <Card style={{rotate: '75deg', top: '30%', left: '20%'}} text='SOLID' containerRef={grid2Container}/>
+                <Card style={{rotate: '-30deg', top: '60%', left: '45%'}} text='GRASP' containerRef={grid2Container}/>
+                <Card style={{rotate: '90deg', top: '30%', left: '70%'}} text='DB' containerRef={grid2Container}/>
+                <Card style={{rotate: '-45deg', top: '55%', left: '0%'}} text='API' containerRef={grid2Container}/>
+                <Card style={{rotate: '20deg', top: '10%', left: '30%'}} text='Design Patterns' containerRef={grid2Container}/>
+                <Card style={{rotate: '30deg', top: '70%', left: '70%'}} image='assets/logos/csharp-pink.png' containerRef={grid2Container}/>
+                <Card style={{rotate: '-45deg', top: '70%', left: '25%'}} image='assets/logos/dotnet-pink.png' containerRef={grid2Container}/>
+                <Card style={{rotate: '-45deg', top: '5%', left: '10%'}} image='assets/logos/blazor-pink.png' containerRef={grid2Container}/>
             </div>
         </div>
+        
         <div className="grid-black-color grid-3"></div>
         <div className="grid-special-color grid-4"></div>
         <div className="grid-default-color grid-5"></div>
